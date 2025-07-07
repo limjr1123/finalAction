0cee6c4f26f073dcc266028fb7507ea867a22258
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-07-07 17:04:08 +0900

    Enemy commit

---
+++ Assets/MEMBER/LJR/Scripts/Enemy/EnemyState/EnemyBattleState.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EnemyBattleState : EnemyState<EnemyController>
+{
+    EnemyController enemy;
+    public override void Enter(EnemyController owner)
+    {
+        enemy = owner;
+        enemy.anim.SetBool("Battle", true);
+    }
+
+    public override void Execute()
+    {
+    }
+}
