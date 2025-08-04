86951cea487235d90868184c45eb4391653d7d4b
KimKyounGs <77844251+KimKyounGs@users.noreply.github.com>
2025-08-04 15:08:02 +0900

    인벤토리 구조 설계
    인벤토리 구조 설계

--- Assets/MEMBER/CKW/Scripts/InGameUI/ToolTipUI.cs
+++ Assets/MEMBER/CKW/Scripts/InGameUI/ToolTipUI.cs
@@ -13,6 +13,8 @@ public class ToolTipUI : MonoBehaviour
     public TextMeshProUGUI itemType;
     public TextMeshProUGUI itemText;
 
+    private ItemData itemData;
+
     void Start()
     {
         if (closeButton != null)
@@ -32,13 +34,44 @@ public class ToolTipUI : MonoBehaviour
 
     void ItemEquip()
     {
+        if (itemData == null) return;
+        if (itemData.ItemType == ItemType.Equipment)
+        {
+            // InventoryManager.Instance.EquipItem(itemData.ItemID, 플레이어 객체);
+        }
+        else if (itemData.ItemType == ItemType.Consumable)
+        {
 
+        }
+        toolTip.SetActive(false);
     }
-
 
     void ItemLift()
     {
-
+        if (itemData == null) return;
+        if (itemData.ItemType == ItemType.Equipment)
+        {
+            // InventoryManager.Instance.UnEquipItem(itemData.ItemID, 플레이어 객체);
+        }
+        toolTip.SetActive(false);
     }
 
+    public void Set(ItemData itemData)
+    {
+        this.itemData = itemData;
+        itemImage.sprite = itemData.ItemSprite;
+        itemName.text = itemData.ItemName;
+        itemType.text = itemData.ItemType.ToString();
+        itemText.text = itemData.Description;
+        toolTip.SetActive(true);
+    }
+
+    public void Clear()
+    {
+        itemImage.sprite = null;
+        itemName.text = null;
+        itemType.text = null;
+        itemText.text = null;
+        toolTip.SetActive(false);
+    }
 }
