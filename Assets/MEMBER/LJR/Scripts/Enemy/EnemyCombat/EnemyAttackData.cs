2d759b03dbdd334189659ad0f70d6a7595511966
limjr1123 <63219254+limjr1123@users.noreply.github.com>
2025-08-06 02:14:42 +0900

    enemy update

--- Assets/MEMBER/LJR/Scripts/Enemy/EnemyCombat/EnemyAttackData.cs
+++ Assets/MEMBER/LJR/Scripts/Enemy/EnemyCombat/EnemyAttackData.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+// 공격 시 사용할 히트박스 종류를 정의하는 열거형입니다.
+public enum AttackHitbox { LeftHand, RightHand, TwoHand, Weapon, LeftFoot, RightFoot }
+
 //Enemy 공격 데이터를 정의하는 스크립트입니다.
 [CreateAssetMenu(menuName = "Combat System/Create a new Attack")]
 public class EnemyAttackData : ScriptableObject
@@ -9,7 +12,16 @@ public class EnemyAttackData : ScriptableObject
     [field: SerializeField] public float impactStartTime { get; private set; }  // 타격 판정이 시작되는 시간 (원거리 공격의 경우 사용되지 않음)
     [field: SerializeField] public float impactEndTime { get; private set; }    // 타격 판정이 끝나는 시간 (원거리 공격의 경우 애니메이션 종료 시간을 입력)
     [field: SerializeField] public bool isParry { get; private set; } // 패링 가능한 공격인지 여부
+
+    [field: SerializeField] public AttackPhase[] attackPhases { get; private set; } // 공격 단계 리스트
 }
 
-// 공격 시 사용할 히트박스 종류를 정의하는 열거형입니다.
-public enum AttackHitbox { LeftHand, RightHand, TwoHand, Weapon, LeftFoot, RightFoot }
+[System.Serializable]
+public struct AttackPhase
+{
+    [SerializeField] public string animName;          // 해당 타격의 애니메이션 이름
+    [SerializeField] public AttackHitbox hitboxToUse; // 사용할 히트박스
+    [SerializeField] public float impactStartTime;    // 타격 판정이 시작되는 시간
+    [SerializeField] public float impactEndTime;      // 타격 판정이 끝나는 시간
+    [SerializeField] public bool isParry;             // 패링 가능한 공격인지 여부
+}
